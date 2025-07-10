374d972320c352afde9e3e198a1c52c4ffde7dbe
Taehwan <165125519+TH-Koo@users.noreply.github.com>
2025-07-10 20:36:59 +0900

    Delete Scripts directory

--- Scripts/QuizData.cs
+++
@@ -1,9 +0,0 @@
-// QuizData.cs
-[System.Serializable]
-public class QuizData
-{
-    public string question;
-    public string option1;
-    public string option2;
-    public int correctOption; // 1 또는 2, 정답이 option1이면 1
-}
